8416b9c33e46ec485141f8859f7d68659153cc8a
Jian Huang <huangjian1993@gmail.com>
2015-07-17 17:49:47 +0800

    lab3A pass basic test

--- src/paxos/paxos.go
+++ src/paxos/paxos.go
@@ -27,10 +27,14 @@ import "log"
 import "os"
 import "syscall"
 import "sync"
+
 import "sync/atomic"
 import "fmt"
+
 import "math/rand"
 
+import "time"
+import "strconv"
 
 // px.Status() return values, indicating
 // whether an agreement has been decided,
@@ -42,6 +46,10 @@ const (
 	Decided   Fate = iota + 1
 	Pending        // not yet decided.
 	Forgotten      // decided but forgotten.
+
+	REJECT  = "REJECT"
+	OK      = "OK"
+	INITNUM = "0"
 )
 
 type Paxos struct {
@@ -53,8 +61,258 @@ type Paxos struct {
 	peers      []string
 	me         int // index into peers[]
 
-
 	// Your data here.
+	instances map[int]*PaxosInstance
+	dones     []int
+}
+
+type Proposal struct {
+	PNum   string
+	PValue interface{}
+}
+
+type PaxosInstance struct {
+	Id       int
+	Decided  bool
+	Prepared string
+	Accepted Proposal
+}
+
+type PaxosArgs struct {
+	Id     int
+	PNum   string
+	PValue interface{}
+
+	Done int
+	Me   int
+}
+
+type PaxosReply struct {
+	Result string
+	PNum   string
+	PValue interface{}
+}
+
+func (px *Paxos) IsMajority(num int) bool {
+	return num > len(px.peers)/2
+}
+
+func (px *Paxos) SelectMajority() []string {
+	total := len(px.peers)
+	size := total/2 + 1
+	size += rand.Intn(total - size)
+
+	hasChoosen := map[int]string{}
+	accetors := make([]string, 0)
+
+	for size > 0 {
+		for {
+			t := rand.Int() % total
+			if _, exist := hasChoosen[t]; !exist {
+				hasChoosen[t] = px.peers[t]
+				accetors = append(accetors, px.peers[t])
+				size--
+				break
+			}
+		}
+	}
+	return accetors
+}
+
+func (px *Paxos) GenerateProposalNumber() string {
+	duration := time.Now().Sub(time.Date(2015, time.July, 10, 10, 26, 0, 0, time.UTC))
+	return strconv.FormatInt(duration.Nanoseconds(), 10) + "-" + strconv.Itoa(px.me)
+}
+
+func (px *Paxos) MakeInstance(id int) {
+	px.instances[id] = &PaxosInstance{
+		Id:       id,
+		Decided:  false,
+		Prepared: INITNUM,
+		Accepted: Proposal{INITNUM, nil},
+	}
+}
+
+func (px *Paxos) ProcessPrepare(args *PaxosArgs, reply *PaxosReply) error {
+	px.mu.Lock()
+	defer px.mu.Unlock()
+	id := args.Id
+	pnum := args.PNum
+	//pvalue := args.PValue
+
+	reply.Result = REJECT
+	if _, exist := px.instances[id]; !exist {
+		px.MakeInstance(id)
+		reply.Result = OK
+	} else {
+		if px.instances[id].Prepared < pnum {
+			reply.Result = OK
+		}
+	}
+	if reply.Result == OK {
+
+		reply.PNum = px.instances[id].Accepted.PNum
+		reply.PValue = px.instances[id].Accepted.PValue
+		px.instances[id].Prepared = pnum
+	}
+	/* if _, exist := px.instances[id]; !exist {*/
+	//px.MakeInstance(id)
+	//reply.Result = OK
+	//}
+	//if px.instances[id].Prepared < pnum {
+	//px.instances[id].Prepared = pnum
+	//px.instances[id].Accepted = Proposal{pnum, pvalue}
+	//reply.Result = OK
+	//}
+	//reply.PNum = px.instances[id].Accepted.PNum
+	//reply.PValue = px.instances[id].Accepted.PValue
+
+	return nil
+}
+
+func (px *Paxos) SendPrepare(id int, clientV interface{}) (bool, []string, Proposal) {
+	pnum := px.GenerateProposalNumber()
+	accetors := px.SelectMajority()
+
+	args := PaxosArgs{
+		Id:     id,
+		PNum:   pnum,
+		PValue: clientV,
+	}
+	reply := PaxosReply{}
+
+	retNum := pnum
+	retValue := clientV
+	success := make([]string, 0)
+	num := 0
+
+	//fmt.Println(accetors, args)
+
+	for _, accetor := range accetors {
+		if accetor == px.peers[px.me] {
+			px.ProcessPrepare(&args, &reply)
+		} else {
+			call(accetor, "Paxos.ProcessPrepare", &args, &reply)
+		}
+		if reply.Result == OK {
+			if reply.PNum > retNum {
+				retNum = reply.PNum
+				retValue = reply.PValue
+			}
+			success = append(success, accetor)
+			num++
+		}
+	}
+	//return IsMajority(len(success)), success, Proposal(retNum, retValue)
+	return px.IsMajority(num), accetors, Proposal{pnum /*retNum*/, retValue}
+}
+
+func (px *Paxos) ProcessAccept(args *PaxosArgs, reply *PaxosReply) error {
+	px.mu.Lock()
+	defer px.mu.Unlock()
+	reply.Result = REJECT
+
+	id := args.Id
+	pnum := args.PNum
+	pvalue := args.PValue
+	if _, exist := px.instances[id]; !exist {
+		px.MakeInstance(id)
+	}
+	if px.instances[id].Prepared <= args.PNum {
+		//if px.instances[id].Accepted.PNum <= args.PNum {
+		px.instances[id].Prepared = pnum
+		px.instances[id].Accepted = Proposal{pnum, pvalue}
+		reply.Result = OK
+	}
+	return nil
+
+}
+
+func (px *Paxos) SendAccept(id int, accetors []string, propsal Proposal) bool {
+	args := PaxosArgs{
+		Id:     id,
+		PNum:   propsal.PNum,
+		PValue: propsal.PValue,
+	}
+	reply := PaxosReply{}
+	num := 0
+
+	for _, accetor := range accetors {
+		if accetor == px.peers[px.me] {
+			px.ProcessAccept(&args, &reply)
+		} else {
+			call(accetor, "Paxos.ProcessAccept", &args, &reply)
+		}
+		if reply.Result == OK {
+			num++
+		}
+	}
+	return px.IsMajority(num)
+}
+
+func (px *Paxos) MakeDecision(id int, result Proposal) {
+	/*px.mu.Lock()*/
+	/*defer px.mu.Unlock()*/
+	if _, exist := px.instances[id]; !exist {
+		px.MakeInstance(id)
+	}
+	px.instances[id].Decided = true
+	px.instances[id].Accepted = result
+}
+
+func (px *Paxos) ProcessDecision(args *PaxosArgs, reply *PaxosReply) error {
+	px.mu.Lock()
+	defer px.mu.Unlock()
+	px.dones[args.Me] = args.Done
+	px.MakeDecision(args.Id, Proposal{args.PNum, args.PValue})
+	return nil
+}
+
+func (px *Paxos) SendDecision(id int, result Proposal) {
+	args := PaxosArgs{
+		Id:     id,
+		PNum:   result.PNum,
+		PValue: result.PValue,
+		Done:   px.dones[px.me],
+		Me:     px.me,
+	}
+	//fmt.Println(args, id, result)
+
+	reply := PaxosReply{}
+	px.MakeDecision(id, result)
+	for i, _ := range px.peers {
+		if i != px.me {
+			call(px.peers[i], "Paxos.ProcessDecision", &args, &reply)
+		}
+	}
+	/* for _, peer := range px.peers {*/
+	////fmt.Println("peer is :", peer)
+	//if peer == px.peers[px.me] {
+	//px.MakeDecision(id, result)
+	//} else {
+	//call(peer, "Paxos.ProcessDecision", &args, &reply)
+	//}
+	/*}*/
+	//fmt.Println("finish send decision")
+}
+
+func (px *Paxos) DoProposal(id int, clientV interface{}) {
+	i := 1
+	for {
+		//fmt.Println(id, clientV)
+		ok, accetors, propsal := px.SendPrepare(id, clientV)
+		//fmt.Println(ok, accetors, propsal)
+		if ok {
+			ok = px.SendAccept(id, accetors, propsal)
+		}
+		//fmt.Println("accept is ok?", ok)
+		if ok {
+			px.SendDecision(id, propsal)
+			//fmt.Println("finish senddecision")
+			break
+		}
+		i++
+	}
 }
 
 //
@@ -93,7 +351,6 @@ func call(srv string, name string, args interface{}, reply interface{}) bool {
 	return false
 }
 
-
 //
 // the application wants paxos to start agreement on
 // instance seq, with proposed value v.
@@ -103,6 +360,13 @@ func call(srv string, name string, args interface{}, reply interface{}) bool {
 //
 func (px *Paxos) Start(seq int, v interface{}) {
 	// Your code here.
+	go func() {
+		//fmt.Println("min is", px.Min(), seq)
+		if seq < px.Min() {
+			return
+		}
+		px.DoProposal(seq, v)
+	}()
 }
 
 //
@@ -113,6 +377,9 @@ func (px *Paxos) Start(seq int, v interface{}) {
 //
 func (px *Paxos) Done(seq int) {
 	// Your code here.
+	if px.dones[px.me] < seq {
+		px.dones[px.me] = seq
+	}
 }
 
 //
@@ -122,7 +389,14 @@ func (px *Paxos) Done(seq int) {
 //
 func (px *Paxos) Max() int {
 	// Your code here.
-	return 0
+	largest := 0
+	for i, _ := range px.instances {
+		if i > largest {
+			largest = i
+		}
+	}
+
+	return largest
 }
 
 //
@@ -155,7 +429,20 @@ func (px *Paxos) Max() int {
 //
 func (px *Paxos) Min() int {
 	// You code here.
-	return 0
+	px.mu.Lock()
+	defer px.mu.Unlock()
+	min := px.dones[px.me]
+	for _, i := range px.dones {
+		if min > i {
+			min = i
+		}
+	}
+	for i, _ := range px.instances {
+		if i <= min && px.instances[i].Decided {
+			delete(px.instances, i)
+		}
+	}
+	return min + 1
 }
 
 //
@@ -167,10 +454,19 @@ func (px *Paxos) Min() int {
 //
 func (px *Paxos) Status(seq int) (Fate, interface{}) {
 	// Your code here.
-	return Pending, nil
+	if seq < px.Min() {
+		return Forgotten, nil
+	}
+	px.mu.Lock()
+	defer px.mu.Unlock()
+	if _, exist := px.instances[seq]; !exist {
+		px.MakeInstance(seq)
+	}
+	if px.instances[seq].Decided == true {
+		return Decided, px.instances[seq].Accepted.PValue
+	}
+	return Pending, px.instances[seq].Accepted.PValue
 }
-
-
 
 //
 // tell the peer to shut itself down.
@@ -214,8 +510,12 @@ func Make(peers []string, me int, rpcs *rpc.Server) *Paxos {
 	px.peers = peers
 	px.me = me
 
-
 	// Your initialization code here.
+	px.instances = map[int]*PaxosInstance{}
+	px.dones = make([]int, len(px.peers))
+	for i, _ := range px.dones {
+		px.dones[i] = -1
+	}
 
 	if rpcs != nil {
 		// caller will create socket &c
@@ -267,7 +567,6 @@ func Make(peers []string, me int, rpcs *rpc.Server) *Paxos {
 			}
 		}()
 	}
-
 
 	return px
 }
